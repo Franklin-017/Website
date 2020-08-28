e0a6d6ba263202022ccf264984347c734a322c42
Franklin-017 <59813186+Franklin-017@users.noreply.github.com>
2020-08-29 02:44:19 +0530

    Responsive Design

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// For visible navbar after hero page
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
@@ -5,4 +6,24 @@ document.addEventListener('scroll', () => {
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
+});
+
+// For opeaning navbar in mobile view
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.navlinks');
+const links = document.querySelectorAll('.navlinks li');
+console.log(links);
+
+hamburger.addEventListener('click', () => {
+	navlinks.classList.toggle('active');
+
+	links.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = '';
+		} else {
+			link.style.animation = `navLinkfade 0.5s ease forwards ${
+				index / 5 + 0.2
+			}s`;
+		}
+	});
 });
